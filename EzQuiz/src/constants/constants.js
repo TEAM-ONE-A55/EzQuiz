ba4849f76ab6bec2e27876e13be42f37542c4f27
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-02-25 16:35:44 +0200

    Add constants and changes in index.css

---
+++ EzQuiz/src/constants/constants.js
@@ -0,0 +1,6 @@
+export const USERNAME_MIN_LENGTH = 3;
+export const USERNAME_MAX_LENGTH = 30;
+export const NAME_MIN_LENGTH = 1;
+export const NAME_MAX_LENGTH = 30;
+export const defaultAvatar = "https://f4.bcbits.com/img/0031181820_25.jpg"
+export const PHONE_NUMBER = 10;
