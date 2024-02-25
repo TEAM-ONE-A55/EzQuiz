6799d664866dcdd61c7baaa6b3275664840a483f
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-02-25 16:33:04 +0200

    Add routes to signin and signup

--- EzQuiz/src/App.jsx
+++ EzQuiz/src/App.jsx
@@ -1,9 +1,19 @@
-import './App.css'
-
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Registration from "./views/Registration/Registration";
+import { Toaster } from "react-hot-toast";
+import Login from "./views/Login/Login";
 export default function App() {
   return (
     <>
+      <BrowserRouter>
+      <Toaster position="bottom-right" reverseOrder={true} />
       <h1>EzQuiz</h1>
+        <Routes>
+          <Route path="/signup" element={<Registration />}></Route>
+          <Route path="/signin" element={<Login />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
