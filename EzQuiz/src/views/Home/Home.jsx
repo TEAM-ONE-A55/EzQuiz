dd4e9eb5fc74d4afa3a91eac694330a83070aa8a
Ivelin Nenov <ivelinnenov9@gmail.com>
2024-02-26 17:40:15 +0200

    Adding onClick on Logo

--- EzQuiz/src/views/Home/Home.jsx
+++ EzQuiz/src/views/Home/Home.jsx
@@ -1,10 +1,12 @@
 import StudentsLandingPage from "../Students Landing Page/StudentsLandingPage";
 import Footer from "../../components/Footer/Footer";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+  const navigate = useNavigate();
   return (
     <div>
-      <h1>EzQuiz</h1>
+      <h1 onClick={() => navigate("/")}>EzQuiz</h1>
       <StudentsLandingPage />
       <Footer/>
     </div>
