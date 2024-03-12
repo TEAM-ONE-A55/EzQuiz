0636fda2e0e91bf54da278a36f3aba6d3fbc393b
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-03-12 18:32:52 +0200

    Added Loader component

---
+++ EzQuiz/src/components/Loader/Loader.jsx
@@ -0,0 +1,20 @@
+import "./Loader.css"
+
+export default function Loader() {
+  return (
+    <div className="lds-spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
