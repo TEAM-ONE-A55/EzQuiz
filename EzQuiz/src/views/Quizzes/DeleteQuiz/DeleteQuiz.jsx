ab10fa0184cd651dde7c8c361a9e5fad3993978a
Georgi Popov <gwvr028@gmail.com>
2024-03-14 20:40:08 +0200

    Styling changes to DeleteQuiz

--- EzQuiz/src/views/Quizzes/DeleteQuiz/DeleteQuiz.jsx
+++ EzQuiz/src/views/Quizzes/DeleteQuiz/DeleteQuiz.jsx
@@ -4,12 +4,12 @@ import { deleteQuizFromDatabase } from "../../../services/quiz.service";
 export default function DeleteQuiz({ id, handle, setChange }) {
     return (
         <button 
-        className=" bg-red-400 text-white text-sm rounded-md max-w-32 px-4 py-2 duration-75 ease-in-out hover:bg-red-500"
+        className=" bg-red-400 font-semibold text-white text-sm rounded-md max-w-32 px-4 py-2 duration-75 ease-in-out hover:bg-red-500"
         onClick={() => {
             deleteQuizFromDatabase(id, handle);
             setChange((prev) => prev + 1);
             }}>
-            Delete Quiz
+            Delete
         </button>
     )
 }
