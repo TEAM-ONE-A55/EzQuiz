c2935c015562c2c9139f0a138fe00811faae7610
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-03-14 20:49:25 +0200

    Remove import from MyQuizzes

--- EzQuiz/src/views/Quizzes/MyQuizzes/MyQuizzes.jsx
+++ EzQuiz/src/views/Quizzes/MyQuizzes/MyQuizzes.jsx
@@ -5,7 +5,6 @@ import { AppContext } from "../../../context/AppContext";
 import { getUserByHandle } from "../../../services/user.service";
 import Button from "../../../components/Button/Button";
 import { useNavigate } from "react-router";
-import { v4 } from "uuid";
 
 export default function MyQuizzes() {
   const { userData } = useContext(AppContext);
