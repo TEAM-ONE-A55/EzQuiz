842f1e75cd90ea69b9711e9fb2f9bf5b40eafec4
Georgi Popov <gwvr028@gmail.com>
2024-03-15 00:08:05 +0200

    Fix bugs

--- EzQuiz/src/views/Quizzes/SimpleQuiz/SimpleQuiz.jsx
+++ EzQuiz/src/views/Quizzes/SimpleQuiz/SimpleQuiz.jsx
@@ -1,10 +1,13 @@
+import { useContext } from "react";
 import { useNavigate } from 'react-router';
 import simpleQuizImage from '../../../images/simple-quiz.jpg';
 import PropTypes from 'prop-types';
 import DeleteQuiz from '../DeleteQuiz/DeleteQuiz';
+import { AppContext } from "../../../context/AppContext";
 
 export default function SimpleQuiz({ quiz, setChange }) {
     const navigate = useNavigate();
+    const { user, userData } = useContext(AppContext);
 
     return (
         <div className=" bg-gray-50 w-64 rounded-xl flex-col h-96 relative shadow-black shadow-xl">
@@ -17,7 +20,8 @@ export default function SimpleQuiz({ quiz, setChange }) {
                         className=" bg-gray-50 border-amber-500 border-2 text-gray-700 hover:text-white font-semibold text-sm rounded-md max-w-32 px-4 py-2 duration-75 ease-in-out hover:bg-amber-500"
                         onClick={() => navigate(`/sample-quiz/${quiz.id}`)}
                         >Take</button>
-                    <DeleteQuiz id={quiz.id} handle={quiz.creator} setChange={setChange}/>
+                    {user && userData.handle === quiz.creator &&
+                        <DeleteQuiz id={quiz.id} handle={quiz.creator} setChange={setChange}/>}
                 </div>
         </div>
     )
