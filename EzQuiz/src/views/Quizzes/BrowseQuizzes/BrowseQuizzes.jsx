61101f7e8470a533091ff9fd5f691bdeb90374d8
Georgi Popov <gwvr028@gmail.com>
2024-03-16 01:05:35 +0200

    Meet a no quizzes case for BrowseQuizzes

--- EzQuiz/src/views/Quizzes/BrowseQuizzes/BrowseQuizzes.jsx
+++ EzQuiz/src/views/Quizzes/BrowseQuizzes/BrowseQuizzes.jsx
@@ -10,13 +10,23 @@ export default function BrowseQuizzes() {
         getAllQuizzesFromDatabase('creator').then(setQuizzes);
     }, [change]);
 
-    return (
-        <div className="grid grid-cols-4 mt-16 max-w-screen-xl m-auto justify-items-center gap-y-16">
-            {quizzes && quizzes.map((quiz) => (
-                <SimpleQuiz key={quiz.id} quiz={quiz} setChange={setChange} />
-            ))}
-        </div>
-    )
+    if (quizzes.length !== 0) {
+        return (
+            <div className="grid grid-cols-4 mt-16 max-w-screen-xl m-auto justify-items-center gap-y-16">
+                {quizzes && quizzes.map((quiz) => (
+                    <SimpleQuiz key={quiz.id} quiz={quiz} setChange={setChange} />
+                ))}
+            </div>
+        )
+    } else {
+        return (
+            <div className='max-w-screen-xl m-auto justify-items-center mt-28'>
+                <h2 className="mb-4 font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-4xl dark:text-white">
+                    No quizzes are available at the moment.<br />Please contact your organiser for further details.
+                </h2>
+            </div>
+        )
+    }
 }
 
   
