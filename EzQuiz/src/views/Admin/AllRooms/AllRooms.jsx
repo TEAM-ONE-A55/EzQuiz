52a004a9e58219018b00fcac5a05a2d4eaf5e85d
Ivelin Nenov <ivelinnenov9@gmail.com>
2024-03-17 14:17:01 +0200

    Adding search in rooms

--- EzQuiz/src/views/Admin/AllRooms/AllRooms.jsx
+++ EzQuiz/src/views/Admin/AllRooms/AllRooms.jsx
@@ -5,6 +5,7 @@ import toast from "react-hot-toast";
 
 export default function AllRooms() {
   const [rooms, setRooms] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const getAllRooms = async () => {
     const allRooms = await getAllHubs("rooms");
@@ -30,28 +31,48 @@ export default function AllRooms() {
       <h1>All Rooms</h1>
       <h2>Total Rooms: {rooms.length}</h2>
       <br />
-      {rooms.map((room) => {
-        if (room) {
-          return (
-            <div key={room.id} className="single-room">
-              <h3>
-                {room.name} <br />
-                Created by {room.creator}
-              </h3>
-              <img src={`${room.image_cover}`} alt="" />
-              {room.participants ? (
-                <div>
-                  <h4>Participants</h4>
-                  {Object.keys(room.participants).length} people in the room
-                </div>
-              ) : (
-                <p>No participants</p>
-              )}
-              <button className="remove-btn" onClick={() => removeRoom(room.id)}>Delete</button>
-            </div>
-          );
-        }
-      })}
+      <div className="flex justify-center">
+        <div className="mb-3 xl:w-96">
+          <input
+            type="search"
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-black outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-neutral-600 focus:outline-none"
+            id="exampleSearch"
+            placeholder="Search groups..."
+          />
+        </div>
+      </div>
+      {rooms
+        .filter((room) =>
+          room.name.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+        .map((room) => {
+          if (room) {
+            return (
+              <div key={room.id} className="single-room">
+                <h3>
+                  {room.name} <br />
+                  Created by {room.creator}
+                </h3>
+                <img src={`${room.image_cover}`} alt="" />
+                {room.participants ? (
+                  <div>
+                    <h4>Participants</h4>
+                    {Object.keys(room.participants).length} people in the room
+                  </div>
+                ) : (
+                  <p>No participants</p>
+                )}
+                <button
+                  className="remove-btn"
+                  onClick={() => removeRoom(room.id)}
+                >
+                  Delete
+                </button>
+              </div>
+            );
+          }
+        })}
     </div>
   );
 }
