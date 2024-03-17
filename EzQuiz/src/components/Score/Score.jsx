724c7caa59b6f12aeeb9e79d74452bab02ed1c4d
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-03-17 09:47:25 +0200

    Add Styling in Score Component

--- EzQuiz/src/components/Score/Score.jsx
+++ EzQuiz/src/components/Score/Score.jsx
@@ -1,4 +1,3 @@
-import Button from "../Button/Button";
 import PropTypes from "prop-types";
 import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router";
@@ -38,55 +37,99 @@ export default function Score({ questions, finishTime, quiz, category }) {
       questions[index].selected_answer === questions[index].correct_answer &&
       questions[index].selected_answer === a
     ) {
-      return "green";
+      return "#68D391";
     } else if (
       questions[index].selected_answer !== questions[index].correct_answer &&
       questions[index].selected_answer === a
     ) {
-      return "#9a1515";
+      return "#FC8181";
     }
     if (questions[index].correct_answer === a) {
-      return "#284632";
+      return "#E1E1E1";
     }
   };
 
   const scorePercent = (+score / (questions.length * 10)) * 100;
 
   return (
-    <div>
-      {quiz && <h1>{category}</h1>}
-      <h1>
-        Your score is {score} from {questions.length * 10}
-      </h1>
-      {Object.values(quiz).length !== 0 &&
-        (scorePercent >= +quiz.passingScore
-          ? `Congratulations! You passed with a score of ${scorePercent}%.`
-          : `You did not pass with score of ${scorePercent}%. To pass, you need a minimum of ${quiz.passingScore}% correct answers.`)}
-      <p>
-        Best time: {minutes} : {seconds}
+    <div className="m-32 min-w-lg">
+      {quiz && category && (
+        <>
+          <h1 className="mb-6 mt-6 font-extrabold leading-none tracking-tight text-neutral-800 md:text-5xl lg:text-5xl">
+            {quiz.title}
+          </h1>
+          <h1 className="mb-6 mt-6  leading-none tracking-tight text-neutral-800 md:text-2xl lg:text-2xl">
+            Category: {category}
+          </h1>
+        </>
+      )}
+      <br />
+      <div className=" w-2/4 bg-yellow-400 mx-auto min-w-lg rounded-xl p-8  shadow-neutral-500 shadow-inner">
+        <h2 className="mb-6 mt-6 font-extrabold leading-none tracking-tight text-neutral-800 md:text-5xl lg:text-5xl">
+          Your score is {score} from {questions.length * 10}
+        </h2>
+        <h3 className="mb-6 mt-6 leading-none tracking-tight text-neutral-800 md:text-2xl lg:text-2xl">
+          {Object.values(quiz).length !== 0 &&
+            (scorePercent >= +quiz.passingScore
+              ? `Congratulations! You passed with a score of ${scorePercent}%.`
+              : `You did not pass with score of ${scorePercent}%. To pass, you need a minimum of ${quiz.passingScore}% correct answers.`)}
+        </h3>
+      </div>
+      <br />
+
+      <p className="mb-6 mt-6 leading-none tracking-tight text-neutral-800 md:text-2xl lg:text-2xl">
+        The quiz was completed in
+        {seconds > 59
+          ? minutes === 1
+            ? ` ${minutes} minutes and ${seconds}.`
+            : ` ${minutes} minutes and ${seconds}.`
+          : ` ${seconds} seconds.`}
       </p>
-      <Button onClick={() => setShowAnswers(!showAnswers)}>
-        View your answers
-      </Button>
-      <Button onClick={() => navigate(-1)}>Close</Button>
+      <br />
+      <button
+        type="button"
+        onClick={() => setShowAnswers(!showAnswers)}
+        data-te-ripple-init
+        data-te-ripple-color="light"
+        className="mb-6 inline-block w-2/4  rounded-lg bg-neutral-800 px-6 pt-2.5 pb-2 text-sm font-medium uppercase leading-normal text-white shadow-lg transition duration-150 ease-in-out hover:bg-neutral-900 hover:shadow-neutral-500 focus:outline-none focus:ring-0 active:bg-neutral-700"
+      >
+        View Your Answers
+      </button>
       {showAnswers &&
         questions.map((q, index) => (
-          <div key={index}>
-            <h3 dangerouslySetInnerHTML={{ __html: q.question }} />
-            {q.mixedAnswers.map((a, indexA) => {
-              return (
-                <div
-                  key={indexA}
-                  className="answer-option"
-                  style={{
-                    backgroundColor: statusAnswers(index, a),
-                  }}
-                  dangerouslySetInnerHTML={{ __html: a }}
+          <div className=" w-2/4 mx-auto" key={index}>
+            <div className="block rounded-xl bg-neutral-100 p-16 text-surface shadow-neutral-500 shadow-lg m-12 mx-auto">
+              <div className=" bg-neutral-100 mx-auto min-w-lg rounded-xl p-8  shadow-neutral-500 shadow-inner">
+                <h3
+                  dangerouslySetInnerHTML={{ __html: q.question }}
+                  className=" text-neutral-800 text-2xl"
                 />
-              );
-            })}
+              </div>
+              {q.mixedAnswers.map((a, indexA) => {
+                return (
+                  <div
+                    key={indexA}
+                    className="answer-option m-2 text-lg rounded-xl bg-neutral-100 p-4 text-neutral-800  shadow-neutral-500 shadow-inner focus:outline-none"
+                    style={{
+                      backgroundColor: statusAnswers(index, a),
+                    }}
+                    dangerouslySetInnerHTML={{ __html: a }}
+                  />
+                );
+              })}
+            </div>
           </div>
         ))}
+      <br />
+      <button
+        type="button"
+        onClick={() => navigate(-1)}
+        data-te-ripple-init
+        data-te-ripple-color="light"
+        className="mb-6 inline-block w-2/4  rounded-lg bg-neutral-50 px-6 pt-2.5 pb-2 text-sm font-medium uppercase leading-normal text-neutral-600 shadow-lg transition duration-150 ease-in-out  focus:outline-none focus:ring-0"
+      >
+        Close
+      </button>
     </div>
   );
 }
