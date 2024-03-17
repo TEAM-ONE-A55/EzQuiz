051758f8cfd4b4c8f1687b0b6568a5c091adbb30
Ivelin Nenov <ivelinnenov9@gmail.com>
2024-03-17 14:16:16 +0200

    Adding admin dashboard

--- EzQuiz/src/views/Admin/AdminDashboard/AdminDashboard.jsx
+++ EzQuiz/src/views/Admin/AdminDashboard/AdminDashboard.jsx
@@ -1,7 +1,89 @@
+import React, { useEffect, useState } from "react";
+import { getAllUsers } from "../../../services/user.service";
+import { getAllHubs } from "../../../services/hub.service";
+import { getAllQuizzesFromDatabase } from "../../../services/quiz.service";
+import { useNavigate } from "react-router";
+
 export default function AdminDashboard() {
-    return (
-        <>
-        <h1>Admin Dashboard</h1>
-        </>
-    )
+  const [users, setUsers] = useState([]);
+  const [rooms, setRooms] = useState([]);
+  const [groups, setGroups] = useState([]);
+  const [quizzes, setQuizzes] = useState([]);
+
+  const navigate = useNavigate();
+
+  const getUsers = async () => {
+    const allUsers = await getAllUsers();
+    setUsers(allUsers);
+  };
+
+  const getQuizzes = async () => {
+    const allQuizzes = await getAllQuizzesFromDatabase("creator");
+    setQuizzes(allQuizzes);
+  };
+
+  const getRooms = async () => {
+    const allRooms = await getAllHubs("rooms");
+    setRooms(allRooms);
+  };
+
+  const getGroups = async () => {
+    const allGroups = await getAllHubs("groups");
+    setGroups(allGroups);
+  };
+
+  useEffect(() => {
+    getUsers();
+    getGroups();
+    getRooms();
+    getQuizzes();
+  }, [users]);
+
+  return (
+    <div>
+      <h2 className="mb-2 mt-0 text-4xl font-medium leading-tight text-primary">
+        Total Users: {users.length}
+      </h2>{" "}
+      <button
+        type="button"
+        onClick={() => navigate("/all-users")}
+        className="inline-block rounded-full bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+      >
+        See all Users
+      </button>{" "}
+      <br />
+      <h2 className="mb-2 mt-0 text-4xl font-medium leading-tight text-primary">
+        Total Quizzes: {quizzes.length}
+      </h2>
+      <button
+        type="button"
+        onClick={() => navigate("/all-quizzes")}
+        className="inline-block rounded-full bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+      >
+        See all Quizzes
+      </button>{" "}
+      <br />
+      <h2 className="mb-2 mt-0 text-4xl font-medium leading-tight text-primary">
+        Total Rooms: {rooms.length}
+      </h2>
+      <button
+        type="button"
+        onClick={() => navigate("/all-rooms")}
+        className="inline-block rounded-full bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+      >
+        See all Rooms
+      </button>{" "}
+      <br />
+      <h2 className="mb-2 mt-0 text-4xl font-medium leading-tight text-primary">
+        Total Groups: {groups.length}
+      </h2>
+      <button
+        type="button"
+        onClick={() => navigate("/all-groups")}
+        className="inline-block rounded-full bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+      >
+        See all Groups
+      </button>
+    </div>
+  );
 }
