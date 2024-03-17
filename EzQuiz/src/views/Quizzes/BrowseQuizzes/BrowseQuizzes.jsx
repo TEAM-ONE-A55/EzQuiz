28a597865426c11bab182fcd9fb6e7a7d3b5c875
Ivelin Nenov <ivelinnenov9@gmail.com>
2024-03-17 14:23:37 +0200

    Adding search to browse quizzes

--- EzQuiz/src/views/Quizzes/BrowseQuizzes/BrowseQuizzes.jsx
+++ EzQuiz/src/views/Quizzes/BrowseQuizzes/BrowseQuizzes.jsx
@@ -1,45 +1,64 @@
-import { useState, useEffect, useContext } from 'react';
-import { getAllQuizzesFromDatabase } from '../../../services/quiz.service';
+import { useState, useEffect, useContext } from "react";
+import { getAllQuizzesFromDatabase } from "../../../services/quiz.service";
 import { AppContext } from "../../../context/AppContext";
-import SimpleQuiz from '../SimpleQuiz/SimpleQuiz';
+import SimpleQuiz from "../SimpleQuiz/SimpleQuiz";
 
 export default function BrowseQuizzes() {
-    const [quizzes, setQuizzes] = useState([]);
-    const [change, setChange] = useState(0);
+  const [quizzes, setQuizzes] = useState([]);
+  const [change, setChange] = useState(0);
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const { user, userData } = useContext(AppContext);
+  const { user, userData } = useContext(AppContext);
 
-    useEffect(() => {
-        getAllQuizzesFromDatabase('creator').then(setQuizzes);
-    }, [change]);
+  useEffect(() => {
+    getAllQuizzesFromDatabase("creator").then(setQuizzes);
+  }, [change]);
 
-    if (quizzes.length !== 0) {
-        return (
-            <div className="grid grid-cols-4 mt-16 max-w-screen-xl m-auto justify-items-center gap-y-16">
-                {quizzes && quizzes
-                .filter((quiz) => {
-                    if (user && userData.role === 'student') {
-                        return quiz.visibility === 'public';
-                    } else if (!user) {
-                        return quiz.visibility === 'public';
-                    } else {
-                        return true;
-                    }
-                })
-                .map((quiz) => (
-                    <SimpleQuiz key={quiz.id} quiz={quiz} setChange={setChange} />
-                ))}
-            </div>
-        )
-    } else {
-        return (
-            <div className='max-w-screen-xl m-auto justify-items-center mt-28'>
-                <h2 className="mb-4 font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-4xl dark:text-white">
-                    No quizzes are available at the moment.<br />Please contact your organiser for further details.
-                </h2>
-            </div>
-        )
-    }
+  if (quizzes.length !== 0) {
+    return (
+      <div>
+        <br />
+        <div className="flex justify-center">
+          <div className="mb-3 xl:w-96">
+            <input
+              type="search"
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-black outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-neutral-600 focus:outline-none"
+              id="exampleSearch"
+              placeholder="Search quizzes..."
+            />
+          </div>
+        </div>
+        <div className="grid grid-cols-4 mt-16 max-w-screen-xl m-auto justify-items-center gap-y-16">
+          {quizzes &&
+            quizzes
+              .filter((quiz) => {
+                if (user && userData.role === "student") {
+                  return quiz.visibility === "public";
+                } else if (!user) {
+                  return quiz.visibility === "public";
+                } else {
+                  return true;
+                }
+              })
+              .filter((quiz) =>
+                quiz.title.toLowerCase().includes(searchTerm.toLowerCase())
+              )
+              .map((quiz) => (
+                <SimpleQuiz key={quiz.id} quiz={quiz} setChange={setChange} />
+              ))}
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="max-w-screen-xl m-auto justify-items-center mt-28">
+        <h2 className="mb-4 font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-4xl dark:text-white">
+          No quizzes are available at the moment.
+          <br />
+          Please contact your organizer for further details.
+        </h2>
+      </div>
+    );
+  }
 }
-
-  
