37b41de4c1834e32a08532df35f45d4095f482d7
Ivelin Nenov <ivelinnenov9@gmail.com>
2024-03-17 23:52:28 +0200

    Implementing Edit Room Name + cosmetic changes

--- EzQuiz/src/views/Admin/AllRooms/AllRooms.jsx
+++ EzQuiz/src/views/Admin/AllRooms/AllRooms.jsx
@@ -2,14 +2,49 @@ import { useEffect, useState } from "react";
 import { deleteHub, getAllHubs } from "../../../services/hub.service";
 import "./AllRooms.css";
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
+import { ref, update } from "firebase/database";
+import { db } from "../../../config/firebase.config";
 
 export default function AllRooms() {
   const [rooms, setRooms] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [editing, setEditing] = useState(false);
+  const [roomName, setRoomName] = useState("");
+  const [roomEditId, setRoomEditId] = useState("");
 
   const getAllRooms = async () => {
     const allRooms = await getAllHubs("rooms");
     setRooms(allRooms);
+  };
+
+  const editRoomName = async (hub, id, key, value) => {
+    const path = `${hub}/${id}/${key}`;
+    return update(ref(db), { [path]: value });
+  };
+
+  const handleChange = (e) => {
+    setRoomName(e.target.value);
+  };
+
+  const handleEdit = (id) => {
+    setEditing(true);
+    setRoomEditId(id);
+  };
+
+  const handleSave = (room, value) => {
+    if (roomName.length < 2) {
+      toast.error("Room name must be at least 2 characters long");
+      return;
+    }
+    setEditing(false);
+    editRoomName("rooms", room.id, "name", value);
+  };
+
+  const handleCancel = () => {
+    setEditing(false);
   };
 
   const removeRoom = async (id) => {
@@ -22,9 +57,15 @@ export default function AllRooms() {
     }
   };
 
+  const navigate = useNavigate();
+
   useEffect(() => {
     getAllRooms();
   }, []);
+
+  useEffect(() => {
+    getAllRooms();
+  }, [editing]);
 
   return (
     <div>
@@ -42,37 +83,95 @@ export default function AllRooms() {
           />
         </div>
       </div>
-      {rooms
-        .filter((room) =>
-          room.name.toLowerCase().includes(searchTerm.toLowerCase())
-        )
-        .map((room) => {
-          if (room) {
-            return (
-              <div key={room.id} className="single-room">
-                <h3>
-                  {room.name} <br />
-                  Created by {room.creator}
-                </h3>
-                <img src={`${room.image_cover}`} alt="" />
-                {room.participants ? (
-                  <div>
-                    <h4>Participants</h4>
-                    {Object.keys(room.participants).length} people in the room
-                  </div>
-                ) : (
-                  <p>No participants</p>
-                )}
-                <button
-                  className="remove-btn"
-                  onClick={() => removeRoom(room.id)}
+      <table className="min-w-full text-left text-sm font-light">
+        <thead className="border-b font-medium dark:border-neutral-500">
+          <tr>
+            <th scope="col" className="px-6 py-4">
+              Room name
+            </th>
+            <th scope="col" className="px-6 py-4">
+              Participants
+            </th>
+            <th scope="col" className="px-6 py-4">
+              Quizzes
+            </th>
+            <th scope="col" className="px-6 py-4">
+              Creator
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {rooms
+            .filter((room) =>
+              room.name.toLowerCase().includes(searchTerm.toLowerCase())
+            )
+            .map((room) => {
+              return (
+                <tr
+                  key={room.id}
+                  className="border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600"
                 >
-                  Delete
-                </button>
-              </div>
-            );
-          }
-        })}
+                  {editing && room.id === roomEditId ? (
+                    <div>
+                      <input
+                        type="text"
+                        value={roomName}
+                        onChange={handleChange}
+                      />
+                      <button
+                        className="hover:cursor-pointer inline-block rounded bg-neutral-800 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-50 shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] transition duration-150 ease-in-out hover:bg-neutral-800 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-neutral-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] dark:bg-neutral-900 dark:shadow-[0_4px_9px_-4px_#030202] dark:hover:bg-neutral-900 dark:hover:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:focus:bg-neutral-900 dark:focus:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:active:bg-neutral-900 dark:active:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)]"
+                        onClick={() => handleSave(room, roomName)}
+                      >
+                        Save
+                      </button>
+                      <button
+                        className="hover:cursor-pointer inline-block rounded bg-neutral-800 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-50 shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] transition duration-150 ease-in-out hover:bg-neutral-800 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-neutral-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] dark:bg-neutral-900 dark:shadow-[0_4px_9px_-4px_#030202] dark:hover:bg-neutral-900 dark:hover:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:focus:bg-neutral-900 dark:focus:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:active:bg-neutral-900 dark:active:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)]"
+                        onClick={handleCancel}
+                      >
+                        Cancel
+                      </button>
+                    </div>
+                  ) : (
+                    <td className="whitespace-nowrap px-6 py-4">
+                      {room.name}{" "}
+                      <FontAwesomeIcon
+                        icon={faPen}
+                        onClick={() => handleEdit(room.id)}
+                      />
+                    </td>
+                  )}
+
+                  <td className="whitespace-nowrap px-6 py-4">
+                    {Object.keys(room.participants).length}
+                  </td>
+                  {room.quizzes ? (
+                    <td className="whitespace-nowrap px-6 py-4">
+                      {Object.keys(room.quizzes).length}
+                    </td>
+                  ) : (
+                    <td className="whitespace-nowrap px-6 py-4">0</td>
+                  )}
+                  <td className="whitespace-nowrap px-6 py-4">
+                    <span
+                      className=" hover:cursor-pointer text-primary transition duration-150 ease-in-out hover:text-primary-600 focus:text-primary-600 active:text-primary-700 dark:text-primary-400 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600"
+                      onClick={() => navigate(`profile/${room.creator}`)}
+                    >
+                      {room.creator}
+                    </span>
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => removeRoom(room.id)}
+                      className=" hover:cursor-pointer inline-block rounded bg-neutral-800 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-50 shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] transition duration-150 ease-in-out hover:bg-neutral-800 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-neutral-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] dark:bg-neutral-900 dark:shadow-[0_4px_9px_-4px_#030202] dark:hover:bg-neutral-900 dark:hover:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:focus:bg-neutral-900 dark:focus:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:active:bg-neutral-900 dark:active:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)]"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
     </div>
   );
 }
