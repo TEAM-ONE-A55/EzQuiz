1396c9dafbf1de71e854620c3be4d6961ea6acc7
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-03-20 06:07:14 +0200

    Add new firebase config

--- EzQuiz/src/config/firebase.config.js
+++ EzQuiz/src/config/firebase.config.js
@@ -4,17 +4,17 @@ import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD81Qd1UtWJ4yUTrLMtq1V8LY7VhCDS4-s",
-  authDomain: "ezquiz-99cdb.firebaseapp.com",
-  projectId: "ezquiz-99cdb",
-  storageBucket: "ezquiz-99cdb.appspot.com",
-  messagingSenderId: "226295104069",
-  appId: "1:226295104069:web:93e5225fe9b0276620592c",
-  databaseURL: "https://ezquiz-99cdb-default-rtdb.europe-west1.firebasedatabase.app/"
+  apiKey: "AIzaSyBk2Gu-vcdFwTBnhMIjcTNCoBTRogf25Gw",
+  authDomain: "ezquiz-b7ac8.firebaseapp.com",
+  projectId: "ezquiz-b7ac8",
+  storageBucket: "ezquiz-b7ac8.appspot.com",
+  messagingSenderId: "592464378395",
+  appId: "1:592464378395:web:81c54f5d885f0938f956be",
+  databaseURL: "https://ezquiz-b7ac8-default-rtdb.europe-west1.firebasedatabase.app/"
+  
 };
 
 export const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 
 export const db = getDatabase(app);
