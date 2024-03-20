61aafe6d5cd14bc8aa60a4b3d19c6163818dc161
Ivelin Nenov <ivelinnenov9@gmail.com>
2024-03-20 13:43:03 +0200

    Adding sorting constant by categories

--- EzQuiz/src/constants/constants.js
+++ EzQuiz/src/constants/constants.js
@@ -74,10 +74,30 @@ export const usersSortingOptions = [
 ];
 
 export const quizSortingOptions = [
-  { label: "Easy", value: "easy" },
-  { label: "Medium", value: "medium" },
-  { label: "Hard", value: "hard" },
   { label: "All Quizzes", value: "all" },
+  { label: "General Knowledge", value: "9" },
+  { label: "Entertainment: Books", value: "10" },
+  { label: "Entertainment: Film", value: "11" },
+  { label: "Entertainment: Music", value: "12" },
+  { label: "Entertainment: Television", value: "14" },
+  { label: "Entertainment: Video Games", value: "15" },
+  { label: "Entertainment: Board Games", value: "16" },
+  { label: "Science & Nature", value: "17" },
+  { label: "Science: Computers", value: "18" },
+  { label: "Science: Mathematics", value: "19" },
+  { label: "Mythology", value: "20" },
+  { label: "Sports", value: "21" },
+  { label: "Geography", value: "22" },
+  { label: "History", value: "23" },
+  { label: "Politics", value: "24" },
+  { label: "Art", value: "25" },
+  { label: "Celebrities", value: "26" },
+  { label: "Animals", value: "27" },
+  { label: "Vehicles", value: "28" },
+  { label: "Entertainment: Comics", value: "29" },
+  { label: "Science: Gadgets", value: "30" },
+  { label: "Entertainment: Japanese Anime & Manga", value: "31" },
+  { label: "Entertainment: Cartoon & Animations", value: "32" },
 ]
 
 export const userSearchOptions = [
